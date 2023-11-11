de504018b4282775c3f91fa6f7cf7f3c653bf6e1
Jiahong Xu <jiahongxu1997@gmail.com>
2023-11-11 13:35:46 -0800

    pin of the parking lot was added to the

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,9 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const parkingLotsRef = db.collection('parkingLots');
 
 // Fetch parking lot data from Firestore
+// ... (previous code)
+
+// Fetch parking lot data from Firestore
 parkingLotsRef.get()
   .then((snapshot) => {
     var surroundingParkingLots = [];
@@ -21,14 +24,52 @@ parkingLotsRef.get()
         lng: data.lng,
         name: data.name,
         status: data.status,
-        status_update:data.last_update,
+        status_update: data.last_update,
         price: data.price
       };
-      //@Kohei can add some map layer here, to make the parking show on the web page
 
       surroundingParkingLots.push(parkingLotInfo);
       console.log('Loaded parking lot:', parkingLotInfo);
     });
 
-  }
-  )
+    // Create markers and add them to the map after data is fetched
+    surroundingParkingLots.forEach((parkingLotInfo) => {
+      var marker = L.marker([parkingLotInfo.lat, parkingLotInfo.lng], {
+        icon: L.icon({
+          iconUrl: '/images/parking-location.png',
+        })
+      });
+
+      var popupContent = parkingLotInfo.status;
+
+      // Attach a click event handler to show the popup on marker click
+      marker.on('click', function () {
+        marker.bindPopup(popupContent).openPopup();
+      });
+
+      marker.addTo(map); // Add marker to the map
+    });
+
+  })
+  .catch((error) => {
+    console.error('Error fetching parking lot data:', error);
+  });
+
+// ... (remaining code)
+
+
+// Get the elements by their ID
+var popupLink = document.getElementById("popup-link");
+var popupWindow = document.getElementById("popup-window");
+var closeButton = document.getElementById("close-button");
+
+// Show the pop-up window when the link is clicked
+popupLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  popupWindow.style.display = "block";
+});
+
+// Hide the pop-up window when the close button is clicked
+closeButton.addEventListener("click", function () {
+  popupWindow.style.display = "none";
+});
